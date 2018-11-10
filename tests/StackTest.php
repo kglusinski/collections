1f1425e7e8999a2e1f254cf81625db43574fd794
Kamil Głusiński <kontakt@webkonstruktor.pl>
2018-11-10 16:08:27 +0100

    test checking empty stack

--- tests/StackTest.php
+++ tests/StackTest.php
@@ -4,8 +4,16 @@ declare(strict_types=1);
 namespace Webkonstruktor\Collection\Test;
 
 use PHPUnit\Framework\TestCase;
+use Webkonstruktor\Collection\Stack;
 
 class StackTest extends TestCase
 {
+    public function testItCanAllowToCheckIfEmpty()
+    {
+        $stackUnderTest = new Stack();
 
+        $actual = $stackUnderTest->isEmpty();
+
+        $this->assertTrue($actual);
+    }
 }
