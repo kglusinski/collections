86d4f4c625ad2b1ab9e5e00b9b0ed34b38b2928e
Kamil Głusiński <kontakt@webkonstruktor.pl>
2018-11-14 12:07:46 +0100

    added checking fail assertion

--- tests/TypeValidatorTest.php
+++ tests/TypeValidatorTest.php
@@ -6,6 +6,7 @@ namespace Webkonstruktor\Collection\Test;
 
 use PHPUnit\Framework\TestCase;
 use Webkonstruktor\Collection\Queue;
+use Webkonstruktor\Collection\Stack;
 use Webkonstruktor\Collection\TypeValidator;
 
 class TypeValidatorTest extends TestCase
@@ -16,9 +17,11 @@ class TypeValidatorTest extends TestCase
 
         $failedValidation = $validatorUnderTest->validate(1, 'string');
         $successValidation = $validatorUnderTest->validate(1, 'integer');
+        $failedObjectValidation = $validatorUnderTest->validate(new Queue(), Stack::class);
         $successObjectValidation = $validatorUnderTest->validate(new Queue(), Queue::class);
 
         $this->assertFalse($failedValidation);
+        $this->assertFalse($failedObjectValidation);
         $this->assertTrue($successValidation);
         $this->assertTrue($successObjectValidation);
     }
